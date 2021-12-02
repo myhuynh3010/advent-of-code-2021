f413746133ee07571a3769f8a45d937748b22bf6
My Huynh <thanhmy3010@gmail.com>
2021-12-02 12:42:06 +0700

    day2part2

---
+++ app/Console/Commands/Day2/Day2Part2.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Console\Commands\Day2;
+
+use Illuminate\Console\Command;
+
+class Day2Part2 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'day2:part2';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $path = storage_path('app/inputs/day2.txt');
+        $inputs = file($path);
+
+        $horizontal = 0;
+        $depth = 0;
+        $aim = 0;
+
+        foreach ($inputs as $input) {
+            $slipDirection = explode(" ", $input);
+            if ($slipDirection[0] === 'forward') {
+                $horizontal +=  (int) $slipDirection[1];
+                $depth += $aim * (int) $slipDirection[1];
+            }
+            if ($slipDirection[0] === 'down') {
+                $aim += (int) $slipDirection[1];
+                // $depth +=  (int) $slipDirection[1];
+            }
+            if ($slipDirection[0] === 'up') {
+                $aim -= (int) $slipDirection[1];
+                // $depth -=  (int) $slipDirection[1];
+            }
+        }
+
+        dd($horizontal * $depth);
+
+        return Command::SUCCESS;
+    }
+}
